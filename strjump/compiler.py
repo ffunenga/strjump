88777fa40259c2926580d64c7e8e50516992c2cb
Filipe Funenga <fmafunenga@gmail.com>
2015-12-16 21:20:02 +0100

    Fixes bugs in python 2.7

--- strjump/compiler.py
+++ strjump/compiler.py
@@ -9,63 +9,62 @@ from . import elements
 
 
 def process(lst):
-    types = [elements.Identifier, elements.Reference, str]
+    types = [str, elements.Reference, elements.Identifier]
     for i, s in enumerate(lst):
-        assert type(s) in types, "type of lst[%d] is unknown (%s)" % (i, type(s))
-    identifiers = [i.identifier for i in lst if type(i) == elements.Identifier]
-    references = [r for r in lst if type(r) == elements.Reference]
+        __flag = any(isinstance(s, t) for t in types)
+        assert __flag, "type of lst[%d] is unknown (%s)" % (i, type(s))
+        #assert type(s) in types, "type of lst[%d] is unknown (%s)" % (i, type(s))
+    identifiers = [i.identifier for i in lst if isinstance(i, elements.Identifier)]
+    references = [r for r in lst if isinstance(r, elements.Reference)]
     for r in references:
         assert r.identifier in identifiers, "reference unknown '%s'" % r.identifier
 
-    # ~4.5 ms
+    # ~0.4 %
     count = 0
     S = []
     idxs_identifiers = []
     for item in lst:
-        item_type = type(item)  # Tests: this is faster than isinstance().
-        if item_type == elements.Reference:
+        if isinstance(item, elements.Reference):
             continue
-        if item_type == elements.Identifier:
+        if isinstance(item, elements.Identifier):
             S.append(count)
             idxs_identifiers.append(item.identifier)
         count += len(item)
     N = len(S)
     router0 = {k: v for v, k in enumerate(idxs_identifiers)}
 
-    # ~36.5 ms
+    # ~3.7 %
     Cr = []
     row = [0] * N
     for item in lst:
-        item_type = type(item)
-        if item_type == str:
+        if isinstance(item, str):
             continue
-        if item_type == elements.Identifier:
+        if isinstance(item, elements.Identifier):
             Cr.append(row)
             continue
-        if item_type == elements.Reference:
+        if isinstance(item, elements.Reference):
             row = row[:]
             row[router0[item.identifier]] = 1
 
-    # ~700 ms
+    # ~71.4 %
     R = [(math.floor(math.log10(si) + 1)) for si in S]
     while True:
         flag = True
         for i, (ri, si, ci) in enumerate(zip(R, S, Cr)):
             si += sum(map(operator.mul, ci, R))
-            si = math.floor(math.log10(si))
+            si = int(math.floor(math.log10(si)))
             si += 1
             R[i] = si
             flag = flag and (ri == si)
         if flag:
             break
 
-    # ~240 ms
+    # ~24.5 %
     CrR = (sum(map(operator.mul, ci, R)) for ci in Cr)
     X = ((si + ci) for si, ci in zip(S, CrR))
     router1 = {k: str(v) for k, v in zip(idxs_identifiers, X)}
 
-    rst = ''.join((item if type(item) == str else
-                   (item.content if type(item) == elements.Identifier
-                                 else router1[item.identifier]))
-                  for item in lst)
+    id_or_ref = lambda i: i.content if isinstance(i, elements.Identifier) else router1[i.identifier]
+    str_or_obj = lambda i: i if type(i) == str else id_or_ref(i)
+    rst = ''.join(map(str_or_obj, lst))
     return rst
